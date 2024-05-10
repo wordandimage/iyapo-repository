7337b27de405346f27b3bf528d64b323abe01975
nchilla <jnico.chilla@gmail.com>
2024-05-10 17:28:52 -0400

    fully templatized cluster and constellation and got artifact pages up and running

--- .eleventy.js
+++ .eleventy.js
@@ -13,31 +13,33 @@ module.exports = function (eleventyConfig) {
       return array.find(item=>item.properties[property].value==value);
     });
 
-    eleventyConfig.addNunjucksFilter("zero_string",function(id){
+    eleventyConfig.addNunjucksFilter("zero_string",function(id,nz=4){
         let i=id;
         let n = 1;
         if ( i >= 100) { n += 2; i /= 100; }
         if ( i >= 10) { n += 1; }
 
-        let zeros=Array(4 - n).fill('0').join('');
+        let zeros=Array(nz - n).fill('0').join('');
 
         return `${zeros}${id}`;
     })
 
 
     eleventyConfig.addNunjucksFilter( "get_constellation_props", function(item,type,cms) { 
-      console.log( item.properties.narrative.value,item.properties.domain.value,item.properties.object.value)  
+      let props=[];
+
       if(type=='manuscript'){
         let artifacts=cms.artifacts
           .filter(a=>a.properties.manuscript_id.value==item.properties.manuscript_id.value)
           .map(a=>{
             return {
+              orientation:'landscape',
               url: `archive/artifacts-cluster/artifact-${a.properties.manuscript_id.value}-${a.properties.artifact_subid.value}`,
               image:a.properties.media.value[0]?`assets/images_for_web/archive/manuscript${a.properties.manuscript_id.value}/artifact${a.properties.artifact_subid.value}/img@@${a.properties.media.value[0]?.name}@@sm`:'' 
             }
           })
         
-        let props=[
+        props=[
           {type:'text',label:'Name',value:item.properties.manuscript_name.value},
           {type:'tags',value:
           item.properties.narrative.value&&item.properties.domain.value&&item.properties.object.value?{
@@ -49,8 +51,25 @@ module.exports = function (eleventyConfig) {
           {type:'relatives',label:'Descendant Artifacts',value:artifacts.length>0?artifacts:false}
         ]
 
-        return props.filter(a=>a.value);
+      }else if(type=='artifact'){
+        let manuscripts=cms.manuscripts
+          .filter(a=>a.properties.manuscript_id.value==item.properties.manuscript_id.value)
+          .map(a=>{
+            return {
+              orientation:'vertical',
+              url: `archive/manuscripts-cluster/manuscript-${a.properties.manuscript_id.value}`,
+              image:a.properties.scan.value[0]?`assets/images_for_web/archive/manuscript${a.properties.manuscript_id.value}/scan@@sm`:'' 
+            }
+          })
+
+        props=[
+          {type:'text',label:'Name',value:item.properties.artifact_name.value},
+          {type:'accordion',label:'Description',value:item.properties.artifact_description.value[0]?.plain_text},
+          {type:'relatives',label:'Ancestor Manuscript',value:manuscripts.length>0?manuscripts:false}
+        ]
       }
+
+      return props.filter(a=>a.value);
     });
 
     eleventyConfig.addNunjucksFilter( "notion_data_sort_int", function(array,property) {    
